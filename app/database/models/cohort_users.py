9314eea90b8b1344c18e60e7bba1d61d303065ab
Kiran Kharade <79803242+kiran-kharade-inflection@users.noreply.github.com>
2023-11-16 22:39:34 +0530

    Update cohort_users.py

--- app/database/models/cohort_users.py
+++ app/database/models/cohort_users.py
@@ -4,17 +4,14 @@ from sqlalchemy_json import mutable_json_type
 from app.common.utils import generate_uuid4
 from app.database.base import Base
 
-class Cohort(Base):
+class CohortUser(Base):
 
-    __tablename__ = "cohorts"
+    __tablename__ = "cohort_users"
 
     id                   = Column(String(36), primary_key=True, index=True, default=generate_uuid4)
     TenantId             = Column(String(36), default=None, nullable=True)
     CohortId             = Column(String(36), default=None, nullable=False)
     UserId               = Column(String(36), default=None, nullable=False)
-    Description          = Column(String(1024), nullable=True)
-    TenantId             = Column(String(36), default=None, nullable=False)
-    Attributes           = Column(mutable_json_type(dbtype=JSONB, nested=True))
     CreatedAt            = Column(DateTime(timezone=True), server_default=func.now())
     UpdatedAt            = Column(DateTime(timezone=True), onupdate=func.now())
 
