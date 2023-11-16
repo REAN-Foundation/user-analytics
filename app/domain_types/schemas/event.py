4fe6924800e61533624308c5a9b24d4e7dff95fc
Kiran Kharade <79803242+kiran-kharade-inflection@users.noreply.github.com>
2023-11-16 22:51:54 +0530

    Update event.py

--- app/domain_types/schemas/event.py
+++ app/domain_types/schemas/event.py
@@ -10,12 +10,7 @@ class EventCreateModel(BaseModel):
     EventType             : str                   = Field(min_length=2, max_length=128, description="Type of the Event")
     Timestamp             : datetime              = Field(default=None, description="Timestamp of the Event")
     Attributes            : Optional[dict | None] = Field(default=None, description="Attributes of the Event")
-    AppVersion            : Optional[str | None]  = Field(default=None, min_length=2, max_length=16, description="App version of the Event")
-    Platform              : Optional[str | None]  = Field(default=None, min_length=2, max_length=16, description="Platform of the Event")
-    DeviceType            : Optional[str | None]  = Field(default=None, min_length=2, max_length=64, description="Device type of the Event")
-    DeviceId              : Optional[str | None]  = Field(default=None, min_length=2, max_length=128, description="Device id of the Event")
     DaysSinceRegistration : Optional[int | None]  = Field(default=None, description="Days since registration of the Event")
-
 
 class EventUpdateModel(BaseModel):
     pass
@@ -30,20 +25,14 @@ class EventSearchFilter(BaseSearchFilter):
     ToDate   : Optional[datetime]
 
 class EventResponseModel(BaseModel):
-    id                    : UUID4                   = Field(description="Id of the Event")
-    UserId                : str                 = Field(description="Id of the User")
+    id                    : UUID4                 = Field(description="Id of the Event")
+    UserId                : UUID4                 = Field(description="Id of the User")
     TenantId              : UUID4                 = Field(description="Tenant Id of the User")
     Action                : str                   = Field(min_length=2, max_length=256, description="Action of the Event")
     EventType             : str                   = Field(min_length=2, max_length=128, description="Type of the Event")
     Timestamp             : datetime              = Field(default=None, description="Timestamp of the Event")
     Attributes            : Optional[dict | None] = Field(default=None, description="Attributes of the Event")
-    AppVersion            : Optional[str | None]  = Field(default=None, min_length=2, max_length=16, description="App version of the Event")
-    Platform              : Optional[str | None]  = Field(default=None, min_length=2, max_length=16, description="Platform of the Event")
-    DeviceType            : Optional[str | None]  = Field(default=None, min_length=2, max_length=64, description="Device type of the Event")
-    DeviceId              : Optional[str | None]  = Field(default=None, min_length=2, max_length=128, description="Device id of the Event")
     DaysSinceRegistration: Optional[int | None]   = Field(default=None, description="Days since registration of the Event")
-
 
 class EventSearchResults(BaseSearchResults):
     Items: List[EventResponseModel] = []
-
