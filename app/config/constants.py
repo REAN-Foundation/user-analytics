5849d3a746522bbab0a63124bb22cea582db4999
Kiran Kharade <79803242+kiran-kharade-inflection@users.noreply.github.com>
2023-11-16 22:38:00 +0530

    Update constants.py

--- app/config/constants.py
+++ app/config/constants.py
@@ -1,4 +1,4 @@
 API_VERSION: str="0.0.1"
 API_PREFIX: str = "/api/v1"
-PORT: int = 12345
-SERVICE_NAME: str = "Order-Management-Service"
+PORT: int = 23456
+SERVICE_NAME: str = "User-Analytics-Service"
