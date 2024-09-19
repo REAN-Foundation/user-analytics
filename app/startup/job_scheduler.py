e9dd3dbb087fa59755e6bc66010ff6d592e9212b
Kiran Kharade <kiran.kharade@reanfoundation.org>
2024-09-19 20:14:44 +0530

    Added job scheduler to the startup pipeline

--- app/startup/job_scheduler.py
+++ app/startup/job_scheduler.py
@@ -4,8 +4,11 @@ from app.database.services.analytics.analysis_service import generate_daily_anal
 
 ###############################################################################
 
+scheduler: BackgroundScheduler = None
+
 # Initialize the APScheduler and start it
 def start_scheduler():
+
     scheduler = BackgroundScheduler()
 
     # Schedule the task - this runs every day at 1:30 (morning)
