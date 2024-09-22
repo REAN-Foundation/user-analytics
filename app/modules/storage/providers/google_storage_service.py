d28e32d0ec00ec4dd2a9d6638dc3b0fe45d92eba
Kiran Kharade <kiran.kharade@reanfoundation.org>
2024-09-22 21:33:47 +0530

    Added GCP storage service as a provider

--- app/modules/storage/providers/google_storage_service.py
+++ app/modules/storage/providers/google_storage_service.py
@@ -1,27 +1,24 @@
 import io
 import os
 from typing import AsyncIterable, BinaryIO
-import boto3
+from google.cloud import storage
 from app.common.utils import print_exception
 
 ###############################################################################
+# Set up authentication by setting the GOOGLE_APPLICATION_CREDENTIALS
+# environment variable to the path of your service account key file,
+# or use from_service_account_json() method.
 
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-region_name = os.getenv('AWS_REGION')
-bucket_name = os.getenv('AWS_BUCKET')
+#bucket_name = os.getenv('GCP_BUCKET')
+bucket_name = 'user-analytics-storage'
 
 ###############################################################################
 
 class AwsS3StorageService:
 
     def __init__(self):
-        self.s3_client = boto3.client(
-            's3',
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key,
-            region_name=region_name
-        )
+        self.client = storage.Client()
+        self.bucket = self.client.get_bucket(bucket_name)
 
     async def upload_local_file(self, storage_location: str, local_file_path: str):
         try:
@@ -31,7 +28,9 @@ class AwsS3StorageService:
 
             file_name = os.path.basename(local_file_path)
             storage_key = f"{storage_location}/{file_name}"
-            self.s3_client.upload_file(local_file_path, bucket_name, storage_key)
+            blob = self.bucket.blob(storage_key)
+            blob.upload_from_filename(local_file_path)
+
             print(f"File '{local_file_path}' uploaded to '{bucket_name}/{storage_key}'")
             return True
         except Exception as e:
@@ -42,8 +41,9 @@ class AwsS3StorageService:
         try:
             storage_key = f"{storage_location}/{file_name}"
             assert hasattr(stream, 'read') and hasattr(stream, 'seek'), "Stream must be a file-like object with read and seek methods"
+            blob = self.bucket.blob(storage_key)
             stream.seek(0)
-            self.s3_client.upload_fileobj(stream, bucket_name, storage_key)
+            blob.upload_from_file(stream)
             print(f"File uploaded successfully to {bucket_name}/{file_name}")
             return True
         except Exception as e:
@@ -53,7 +53,10 @@ class AwsS3StorageService:
     async def upload_file_as_object(self, storage_location: str, content, file_name: str):
         try:
             storage_key = f"{storage_location}/{file_name}"
-            self.s3_client.put_object(Body=content, Bucket=bucket_name, Key=storage_key)
+            buffer: io.BytesIO = io.BytesIO(content)
+            buffer.seek(0)
+            blob = self.bucket.blob(storage_key)
+            blob.upload_from_file(content)
             print(f"File uploaded to {bucket_name}/{file_name}")
             return True
         except Exception as e:
@@ -64,30 +67,27 @@ class AwsS3StorageService:
         try:
             # Make sure the file path directory exists
             os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
-            self.s3_client.download_file(bucket_name, storage_key, local_file_path)
+
+            blob = self.bucket.blob(storage_key)
+            with open(local_file_path, "wb") as file:
+                blob.download_to_file(file)
+
+            # Or altenatively
+            # blob.download_to_filename(local_file_path)
+
             print(f"File downloaded from {bucket_name}/{storage_key} to {local_file_path}")
+
             return True
         except Exception as e:
             print_exception(e)
             return False
 
-    async def download_file_as_stream(self, storage_key: str):
+    async def download_file_as_stream(self, storage_key: str) -> AsyncIterable[bytes]:
         try:
-            response = self.s3_client.get_object(Bucket=bucket_name, Key=storage_key)
-            stream = response['Body'] # This is a StreamingBody object
-
-            # Or altenative implementation
-
-            # if 'Body' not in response or response['Body'] is None:
-            #     raise ValueError("Failed to retrieve file body from S3")
-            # async def stream_file() -> AsyncIterable[bytes]:
-            #     while True:
-            #         chunk = response['Body'].read(1024)  # Read in chunks of 1024 bytes
-            #         if not chunk:
-            #             break
-            #         yield chunk
-            # stream = stream_file()
-
+            blob = self.bucket.blob(storage_key)
+            stream = io.BytesIO()
+            blob.download_to_file(stream)
+            stream.seek(0)
             print(f"File {storage_key} downloaded from {bucket_name} as stream")
             return stream
         except Exception as e:
@@ -96,13 +96,12 @@ class AwsS3StorageService:
 
     async def download_file_as_object(self, storage_key: str) -> io.BytesIO:
         try:
+            blob = self.bucket.blob(storage_key)
             buffer = io.BytesIO()
-            self.s3_client.download_fileobj(Bucket=bucket_name, Key=storage_key, Fileobj=buffer)
+            blob.download_to_file(buffer)
             buffer.seek(0)
             print(f"File {storage_key} downloaded from {bucket_name} as object")
             return buffer
         except Exception as e:
             print_exception(e)
             return None
-
-###############################################################################
